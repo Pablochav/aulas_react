9a5fe9cef35862a2f587885d21bc602e655dabce
EA - Pablo Chaves De Oliveira <pablo_chaves@educadventista.org>
2023-09-22 15:40:42 -0300

    projeto finalizado

--- 2_MENU_RESPONSIVO/src/App.jsx
+++ 2_MENU_RESPONSIVO/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div>
       <Header />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore natus assumenda quidem omnis, reprehenderit suscipit delectus eligendi dolorem quasi hic minima fugiat, dolorum, ex fugit exercitationem unde deserunt quis dolores.
+      </p>
     </div>
   )
 }
--- 2_MENU_RESPONSIVO/src/components/Header.jsx
+++ 2_MENU_RESPONSIVO/src/components/Header.jsx
@@ -1,15 +1,60 @@
-import{ List } from "@phosphor-icons/react"
+import{ List, X } from "@phosphor-icons/react"
+import styles from "./Header.module.css"
+import { useState } from "react"
+
 
 function Header() {
+ const [abrirMenu, setAbrirMenu] = useState(false)
+
+ function handleAbrirFecharMenu(){
+  if (abrirMenu === true){
+    setAbrirMenu(false)
+    return
+  }
+
+  setAbrirMenu(true)
+ }
+
   return (
-    <header>
-        <div className="mobile">
-            <div className="manu_logo">
-                <button>
+
+    <header className={styles.header}>
+        <div className={styles.mobile}>
+            <div className={styles.menu_logo}>
+                <button onClick={handleAbrirFecharMenu}>
+                  {
+                    abrirMenu === true ? (
+                      <X  size ={32} />
+                    ) : ( 
                     <List size ={32} />
+                    )
+                  }
                 </button>
+
                 <h1>Logo</h1>
             </div>
+
+            <div className={`${styles.options} ${abrirMenu === true && styles.open}`}>
+              <nav className="">
+                <a href="">Home</a>
+                <a href="">Ferramentas</a>
+                <a href="">Objetivos</a>
+                <a href="">Bibliografia</a>
+              </nav>
+            </div>
+        </div>
+
+        <div className={styles.desktop}>
+          <div>
+            <h1>Logo</h1>
+          </div>
+          <div >
+              <nav className={styles.desktop_option}>
+                <a href="">Home</a>
+                <a href="">Ferramentas</a>
+                <a href="">Objetivos</a>
+                <a href="">Bibliografia</a>
+              </nav>
+          </div>
         </div>
     </header>
   )
